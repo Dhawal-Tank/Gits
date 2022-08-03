f862674a6e55be4885790d4b1bfb12975d861069
Dhawal Tank <imdhawaltank@gmail.com>
2022-08-03 12:23:57 +0530

    added implementation for diff.cpp

---
+++ diff.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int x = 200;
+    int y = 100;
+    int z = 75;
+
+    if(x<y){
+        cout << y;
+    }
+    else if (y>z)
+    {
+        cout << z;
+    }
+    else{
+        cout << x;
+    }
+    
+}
